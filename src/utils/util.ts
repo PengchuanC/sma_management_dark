f26cdc57e5ee5a9ed5b53715e27a4962262ee5c3
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2021-10-20 16:44:26 +0800

    refactor(区间收益):
    1.区间收益算法重构
    2.持仓页面展示份额加权区间收益
    3.bug fix

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -8,7 +8,7 @@ export function numeralNum(num: number) {
   if (Math.abs(num) <= 2) {
     return numeral(num).format('0.0000');
   }
-  return numeral(num).format('0,0.00');
+  return numeral(num).format('0,0.0');
 }
 
 export function formatPercent(num: number) {
